27df0fc4c02afb15e5208f1a6088de12d5af5d59
Your Name <you@example.com>
2023-02-24 13:17:07 +0300

    Task3

---
+++ seminar1/project2/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите программу, которая на вход принимает два числа и проверяет является ли первое число квадраом второго
+Console.WriteLine("введите первое число");
+int numa = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int numb = int.Parse(Console.ReadLine());
+int numasqr = numa*numb;
+if(numasqr  == numb)
+{
+    Console.WriteLine("Квадрат перврго числа равен второму числу");
+}
+    else 
+{    Console.WriteLine("Квадрат перврго числа равен второму числу");
+}
+
+
+
---
+++ seminar1/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// напишите программу, которая на вход принимает два числа и проверяет является ли первое число квадраом второго
+Console.WriteLine("День недели");
+int day = int.Parse(Console.ReadLine());
+switch(day)
+{
+    case(1):
+Console.Write ("понедельник");
+break;
+case(2):
+Console.Write ("вторник");
+break;
+case(3):
+Console.Write ("среда");
+break;
+case(4):
+Console.Write ("четверг");
+break;
+case(5):
+Console.Write ("пятница");
+break;
+case(6):
+Console.Write ("суббота");
+break;
+case(7):
+Console.Write ("суббота");
+break;
+default:
+Console.Write ("такого дня не существует на планете Земля");
+break;
+}
