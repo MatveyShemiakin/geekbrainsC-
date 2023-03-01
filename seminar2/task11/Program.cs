dc4dc1d6498e5195b64853acc7bb5fcfc5c253e2
Your Name <you@example.com>
2023-03-01 20:03:50 +0300

    Случайное число на отрезке

---
+++ seminar2/task11/Program.cs
@@ -0,0 +1,19 @@
+﻿// случайное число на отрезке;
+
+int number = new Random().Next(10,100);
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (int FirstDigit > secondDigit)
+{
+    Console.WriteLine(secondDigit);
+}
+else
+{
+     Console.WriteLine(firstDigit);
+}
+
+
+
+
