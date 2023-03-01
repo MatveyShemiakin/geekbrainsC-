bd76080b50e98f997677c517d452c9794c4c3447
Your Name <you@example.com>
2023-03-01 21:10:28 +0300

    трехзначное и отображение второй цифры

---
+++ seminar2/task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//Вариант 1
+
+Console.WriteLine ("Введите 3-х значное число:");
+int num = int.Parse(Console.ReadLine()??"0");
+
+double digit = Math.Log10(num);
+digit = (int)digit;
+
+if (digit == 2)
+{
+    
+    int result = num / 10;
+    result = result%10;
+    Console.WriteLine ("Вторая цифра числа: " + num + " равна: " + result);
+}
+else
+{
+    Console.WriteLine ("Введенное число не 3-х значное!!!");
+    Console.WriteLine ("Введите 3-х значное!!! число!!!");
+}
