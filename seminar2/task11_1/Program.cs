9ee840b0b39bb811d0e4ea7a32ca15ecf931f1f9
Your Name <you@example.com>
2023-03-01 20:34:18 +0300

    трехзначное и удаление цифры

---
+++ seminar2/task11_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = (int)(Math.random() * 1000);
+        int firstDigit = x / 100;
+        int thirdDigit = x % 10;
+        Console.WriteLine(firstDigit + "" + thirdDigit);
+    }
+};
